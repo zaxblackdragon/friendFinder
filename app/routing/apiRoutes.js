3bf3adea64c3d5ef190e2a64cafe9bb0d7b9bed6
Zac Seewald <zaxblackdragon.com>
2018-12-05 20:32:37 -0600

    json route working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,6 +2,6 @@ const path = require("path");
 
 module.exports = function(app){
     app.get("/json", function(req, res) {
-        res.send("Hi there");
+        res.sendFile(path.join(__dirname, "../data/friends.js"));
     });
 };
